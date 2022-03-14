788ce2d4a3f947c4dc300b65830754d159764897
SavashSyuleyman <admin@sava.sh>
2022-03-14 21:12:50 +0200

    alers

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -8,3 +8,22 @@ class AlertController extends Controller
 {
     //
 }
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlertController extends Controller
+{
+    //
+    publiic function index()
+    {
+
+    }
+
+    public function add_annotation()
+    {
+        
+    }
+}
